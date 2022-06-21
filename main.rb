f2eb4f2b514de1d24665b1e57f20a2d3bddfa776
nanarama <annasampers@gmail.com>
2022-06-20 21:26:57 -0500

    GameLogic module with get_feedback method broken out. Used rlmoser99's code as template for creating the compare_previous_guesses method. ComputerGuesser now wins!

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 module Display
 
-  COLORS = "1. Red\n 2. Yellow\n 3. Blue\n 4. Green\n 5. Pink\n 6. White"
+  COLORS = "1. Red 2. Yellow 3. Blue 4. Green 5. Pink 6. White"
   COLOR_CODES = ["R", "Y", "B", "G", "P", "W"]
 
   def start_game
@@ -8,24 +8,17 @@ module Display
   end
 
   def rules_for_human_guesser
-    puts "There are 6 available colors.\n 
-    The computer will randomly select a combination of the colors (duplicates are allowed).\n
-    You have 12 turns to guess the combination in the correct order.\n
-    'O' = means you've guessed the correct color in the correct spot.\n
-    '?' = means you've guessed the correct color but wrong spot.\n
-    'X' = means that color is not part of the answer."
+    puts "There are 6 available colors.
+The computer will randomly select a combination of the colors (duplicates are allowed).
+You have 12 turns to guess the combination in the correct order.\n"
     puts "The available colors are:\n #{COLORS}"
   end
 
   def rules_for_computer_guesser
     puts "There are 6 available colors."
     puts "#{COLORS}"
-    puts "You will select a combination of these colors (duplicates are allowed)\n
-    to place in 4 secret slots. The computer will have 12 turns to guess the 
-    combination in the correct order.\n
-    'O' = means it guessed the correct color in the correct spot.\n
-    '?' = means it guessed the correct color but wrong spot.\n
-    'X' = means that color is not part of the answer."
+    puts "You will select a combination of these colors (duplicates are allowed)
+to place in 4 secret slots. The computer will have 12 turns to guess the combination in the correct order."
   end
 
   def lost
@@ -37,14 +30,54 @@ module Display
   end
 end 
 
+module GameLogic
+  def get_feedback(answer, current_guess)
+    temp_answer = answer.clone
+    temp_guess = current_guess.clone
+    @exact_num = exact_matches(temp_answer, temp_guess)
+    @same_num = include_colors(temp_answer, temp_guess)  
+    return @exact_num, @same_num
+  end
+ 
+  def exact_matches(answer, current_guess)
+    exact = 0
+    answer.each_with_index.map do |item, i| 
+      if current_guess[i] == answer[i]
+        current_guess[i] = "X"
+        answer[i] = "#{item}+"
+        exact += 1 
+      end
+    end
+    exact
+  end
+
+  def include_colors(answer, current_guess)
+    same = 0
+    current_guess.each_with_index.map do |item, i|
+      if answer.include?(current_guess[i]) 
+        same += 1
+        remove = answer.find_index(current_guess[i])
+        answer[remove] = "?"
+        current_guess[i] = "-"
+      end 
+    end 
+    same 
+  end
+
+end
+
 class Game 
   include Display
-
+  include GameLogic
 
   def initialize(players)
+    if [HumanGuesser, ComputerSelector]
+      rules_for_human_guesser
+    elsif 
+      rules_for_computer_guesser
+    end    
     @answer = players.players[1].new.pick_answer
     p @answer.join
-    #p @answer.uniq
     @guesser = players.players[0].new
     @current_guess = ''
   end
@@ -52,61 +85,41 @@ class Game
   def play
     loop do
       get_guess(@guesser)
-      display_feedback(@current_guess, @answer)
+      display_feedback(@answer, @current_guess) 
 
-      if player_has_won?(@current_guess)
-        puts "#{@answer.join}: Code"
-        won()
-        return
-      elsif @guesser.total_guesses == 12
-        puts "#{@answer.join}: Code"
-        lost()
-        return 
-      end
+        if player_has_won?(@current_guess)
+          puts "#{@answer.join}: Code"
+          won()
+          return
+        elsif @guesser.total_guesses == 12
+          puts "#{@answer.join}: Code"
+          lost()
+          return 
+        end
 
       clear_guess()
     end 
   end
 
+  def display_feedback(answer, current_guess)
+    get_feedback(answer, current_guess)
+    if @exact_num <= 3 && @exact_num > 0
+        puts "#{@exact_num}: Correct"
+      end 
+    if @same_num > 0
+      puts "#{@same_num}: Right color, wrong spot"
+    end 
+    if @guesser.to_s == "ComputerGuesser" 
+        @guesser.all_guesses << [current_guess, @exact_num, @same_num]
+      end 
+    return @exact_num, @same_num 
+  end
+
   def get_guess(guesser)
-    @current_guess = guesser.make_guess
-  end
-
-
-  def display_feedback(current_guess, answer)
-    @feedback = Array(current_guess.split(''))
-    @correct_colors = 0
-    @half_correct_colors = 0
-    get_feedback(@feedback, answer, @correct_colors, @half_correct_colors)
-    return current_guess
-  end
-
-  def get_feedback(feedback, answer, correct_colors, half_correct_colors)
-    answer.each_with_index.map do |item, i| 
-      if feedback[i] == answer[i]
-        feedback[i] = "X"
-        answer[i] = "#{item}+"
-        correct_colors += 1 
-      end
+    if guesser.to_s == "ComputerGuesser" && guesser.total_guesses > 1
+      guesser.compare_previous_guesses
     end
-
-  feedback.each_with_index.map do |item, i|
-    if feedback.include?(answer[i]) 
-      half_correct_colors += 1
-    end 
-  end
-
-    answer.map do |item|
-      item.gsub!("+", "")
-    end  
-
-    if correct_colors <= 3 && correct_colors > 0
-      puts "#{correct_colors}: Correct"
-    end 
-    if half_correct_colors > 0
-      puts "#{half_correct_colors}: Right color, wrong spot"
-    end  
-    return feedback
+    @current_guess = guesser.make_guess
   end
 
   def clear_guess()
@@ -114,16 +127,18 @@ class Game
   end
 
   def player_has_won?(current_guess)
-    @answer.join == current_guess
+    @answer.join == current_guess.join
   end
 
 end
 
 class Player 
+include Display
 
   attr_reader :players
 
   def initialize
+    start_game
     choose_role()
   end
 
@@ -153,6 +168,7 @@ end
 
 class HumanGuesser 
   include Display
+
   attr_accessor :total_guesses
 
   def initialize
@@ -164,7 +180,7 @@ class HumanGuesser
     puts "Enter guess ##{@total_guesses}."
     @guess = gets.chomp.upcase
     @total_guesses += 1
-    return @guess
+    return Array(@guess.split(''))
   end
 
 end
@@ -180,25 +196,57 @@ class ComputerSelector
     @answer[3] = COLOR_CODES.sample
     @answer
   end
-
-
 end
 
 class ComputerGuesser 
   include Display
-  attr_accessor :total_guesses
+  include GameLogic
+  attr_reader :exact_num, :same_num, :total_num
+  attr_accessor :total_guesses, :all_combinations, :all_guesses
 
   def initialize
     @total_guesses = 1
+    @all_combinations = COLOR_CODES.repeated_permutation(4).to_a
+    @all_guesses = []
+    @last_guess = []
   end
 
   def make_guess
     puts "Options: #{COLOR_CODES.join(", ")}"
     puts "Enter guess ##{@total_guesses}."
-    @guess = COLOR_CODES.sample + COLOR_CODES.sample + COLOR_CODES.sample + COLOR_CODES.sample
-    puts @guess
+    @guess = @all_combinations.sample
+    @last_guess = @guess.clone.join
+    puts "The last guess is #{@last_guess}"
+    puts @guess.join
     @total_guesses += 1
+    puts "Total # of combinations is #{@all_combinations.length}"
+    puts "All guesses #{@all_guesses}"
     return @guess
+  end
+
+  def compare_previous_guesses
+    @all_guesses.each {|code| run_through(code)}
+  end  
+
+  def run_through(code)
+    compare_guesses(code[0], code[1], code[2])
+  end
+
+  def compare_guesses(code, exact, same)
+    @all_combinations.each do |option|
+      get_feedback(option, code)
+      reduce_options(option) unless @exact_num == exact && @same_num == same
+    end  
+  end
+
+  def reduce_options(guess)
+    @all_combinations.reject! do |option|
+      option == guess
+    end  
+  end
+
+  def to_s
+    "ComputerGuesser"
   end
 
 end
@@ -212,7 +260,6 @@ class HumanSelector
     @answer = gets.chomp.upcase
     @answer = Array(@answer.split(''))
   end
-
 
 end
 
